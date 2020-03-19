176452d8ee4abc6ab7a26d393918369736efae13
journeyman32 <bryan@coderbox.co>
2020-03-18 20:35:48 -0500

    3.48 Two way binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,20 +1,25 @@
 
 import { Component } from '@angular/core';
+// import { NgModel} from '@angular/forms';
 
 
 @Component({
   selector: 'courses',
   template: `
-
-   <input #email (keyup.enter)="onKeyup(email.value)"/>
+<!-- not the best method for two way binding -->
+   <input [value]="email" (keyup.enter)="email = $event.target.value; onKeyup()"/>
+<!-- better way for two way binding useing the two way binding syntax -->
+   <input [(ngModel)]="email" (keyup.enter)="onKeyup()"/>
 
  `
 })
 
 export class CoursesComponent{
 
-onKeyup(email){
-  console.log(email);
+email = "me@example.com";
+
+onKeyup(){
+  console.log(this.email);
 }
 
 }
