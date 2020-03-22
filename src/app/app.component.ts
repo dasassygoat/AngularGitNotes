a31046cd3f846d889542cf3cd7ec2c8ef366accd
journeyman32 <bryan@coderbox.co>
2020-03-21 22:42:04 -0500

    6.71 NgClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,9 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses;
-
-
-  loadCourses(){
-this.courses = [
-  {id:1,name:'course1'},
-  {id:2,name:'course2'},
-  {id:3,name:'course3'}
-];
-  }
-
-  trackCourse(index, course){
-    return course ? course.id : undefined;
+  isEmpty = false;
+  onClick(){
+    this.isEmpty = !this.isEmpty;
   }
 }
 
