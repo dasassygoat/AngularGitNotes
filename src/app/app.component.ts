a6323d0a18f95fa6bcfe613fc21b5e9cc0c3cdcd
journeyman32 <bryan@coderbox.co>
2020-03-21 22:23:28 -0500

    6.68 NgFor change detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,20 @@ export class AppComponent {
     {id:2,name:'course2'},
     {id:3,name:'course3'}
   ];
+clickTimes = 4;
+
+onAdd() {
+    this.courses.push({id:this.clickTimes,name:'course'+ this.clickTimes});
+    this.clickTimes++;
+  }
+
+  onRemove(course) {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index);
+  }
+
+  onChange(course){
+    course.name = "UPDATED";
+  }
 }
 
